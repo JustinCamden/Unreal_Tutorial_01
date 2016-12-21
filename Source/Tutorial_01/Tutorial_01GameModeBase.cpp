4715b4d709a2c5f892746ad0b484c36b3d53b43a
JustinCamden <camden@usc.edu>
2016-12-20 22:35:12 -0800

    Initialization

---
+++ Source/Tutorial_01/Tutorial_01GameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Tutorial_01.h"
+#include "Tutorial_01GameModeBase.h"
+
+
+
+
---
+++ Source/Tutorial_01/Tutorial_01GameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "Tutorial_01GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TUTORIAL_01_API ATutorial_01GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
